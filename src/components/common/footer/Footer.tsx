1ef6d8c29f3ad5c98daffe62de378a6156589e16
klezovich <arthur@trevor.io>
2023-05-05 15:32:04 +0200

    Fix dependency versions

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import {makeStyles, Theme} from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme:Theme) => ({
-    root: {
-        flexGrow: 1,
-        marginTop: theme.spacing(2),
-        backgroundColor: theme.palette.primary.main,
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
 
 const Footer = () => {
-    const classes = useStyles();
 
     return (
-        <footer className={classes.root}>
+        <footer>
             <p>This a footer</p>
         </footer>
     );
