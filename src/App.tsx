e577764e09feab6e33fd672f1a1567d82f3ba940
klezovich <arthur@trevor.io>
2023-05-05 14:33:58 +0200

    Add basic website

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/img/rocket.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img style={{width: '200px', height:'200px'}} src={logo} alt="logo"/>
+            <p>Prepare to fly on the IT-rocket!</p>
+            <p>This will be a great journey!</p>
+        </div>
+    );
 }
 
 export default App;
